0bc994b3b2314a05dc93fd9af82cc0a6f6535707
lixl <lixiaolong1@shihuituan.com>
2021-06-17 16:00:48 +0800

    application

--- src/main/java/com/lixl/mybatis/demo/SpringDisruptorApplication.java
+++ src/main/java/com/lixl/mybatis/demo/SpringDisruptorApplication.java
@@ -1,14 +1,13 @@
 package com.lixl.mybatis.demo;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SrpingMybatisApplication {
+public class SpringDisruptorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SrpingMybatisApplication.class, args);
+        SpringApplication.run(SpringDisruptorApplication.class, args);
     }
 
 }
--- src/test/java/com/lixl/mybatis/demo/SpringDisruptorApplicationTests.java
+++ src/test/java/com/lixl/mybatis/demo/SpringDisruptorApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SrpingMybatisApplicationTests {
+class SpringDisruptorApplicationTests {
 
     @Test
     void contextLoads() {
